2d762003e1f3d867e68d73fe1dade44748141b51
刘伟鹏 <liu.wp@inspur.com>
2018-04-27 11:36:55 +0800

    init commit

---
+++ org/wayne/packages.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func add()  {
+	
+}
+
+func main() {
+
+	fmt.Println("My favorite number is", rand.Intn(100))
+//方法名首字母大写，为导出包
+	fmt.Println(math.Pi)
+
+
+}
